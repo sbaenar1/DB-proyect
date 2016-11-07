b09db30ac322dbed96a9b3533300951f51cd7cd6
David Sanchez <davidsanchezuribe@hotmail.com>
2016-11-07 11:53:01 -0500

    Avanzando en vista Detalle

--- Workbench/Vistas/Participacion_vDetalle.php
+++ Workbench/Vistas/Participacion_vDetalle.php
@@ -22,7 +22,7 @@
 					echo "<option value='e" . $row['idEventos'] . "'>" . $row['nombre'] . "</option>";
 				} 
 				echo '</select>';
-				?>
+				?> 	<input type="submit" name="Buscar" value="Buscar"> <br>
 
 				<?php
 				include 'conexion.php';
@@ -39,7 +39,42 @@
 
 			</fieldset>
 		</form>
+		<h2>Artistas que participan en estos eventos</h2>
 		<?php
+		include 'conexion.php';
+
+		if(isset($_GET['Buscar'])){
+			$evento = (int)(substr($_GET['evento'], 1));
+
+			$result=mysqli_query($connect, "SELECT nombre 
+				FROM Eventos WHERE idEventos = '$evento'");
+			$row = mysqli_fetch_assoc($result);
+			$nombre = $row["nombre"];
+
+			echo '<form action="" method="get" >
+			<fieldset>
+				<legend>Evento: ' . $nombre . '</legend>';
+
+				$get=mysqli_query($connect, "SELECT idArtistas, nombre 
+				FROM Participacion INNER JOIN Artistas
+				ON Artistas.idArtistas = Participacion.Artistas_idArtistas
+				WHERE Eventos_idEventos = '$evento'");
+
+				echo 'Artistas:<select name="artista">';
+				while ($row = $get->fetch_assoc()){
+					echo "<option value='a" . $row['idArtistas'] . "'>" . $row['nombre'] . "</option>";
+				} 
+				echo '</select>';
+
+
+			echo '</fieldset>
+		</form>';
+
+			/*echo 'Seleccione un Artista:<select name="artista" id="artista">';
+			while ($row = $get->fetch_assoc()){
+				echo "<option value='a" . $row['idArtistas'] . "'>" . $row['nombre'] . "</option>";
+			}*/ 
+		}/*
 		include 'conexion.php';
 		if(isset($_GET['Agregar'])){
 			echo $evento = substr($_GET['evento'], 1);
@@ -63,6 +98,9 @@
 			}
 		}
 
+
+
+*/
 		mysqli_close($link);
 		?>
 	</body>
